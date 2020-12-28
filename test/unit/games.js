30ce9889392e5c2c44f334333883b03c8d88aba1
Goel Biju <goelmolel@hotmail.com>
2020-12-28 11:31:12 +0000

    Attempt to fix tests - 9 (#39)

--- test/unit/games.js
+++ test/unit/games.js
@@ -85,85 +85,85 @@ suite("Game - Controller/Model", () => {
     chai.expect(cntTwo).to.eq(2);
   });
 
-  // test("gets a random player from a game", async () => {
-  //   // Add some players to the game
-  //   await Game.findById("1", async (err, game) => {
-  //     game.players = ["player 1", "player 2"];
-  //     await game.save();
-  //   });
+  test("gets a random player from a game", async () => {
+    // Add some players to the game
+    await Game.findById("1", async (err, game) => {
+      game.players = ["player 1", "player 2"];
+      await game.save();
+    });
 
-  //   let counter = 0;
-  //   for (let i = 0; i < 100; i++) {
-  //     let p = await getRandomPlayer("1");
+    let counter = 0;
+    for (let i = 0; i < 100; i++) {
+      let p = await getRandomPlayer("1");
 
-  //     chai.assert.include([0, 1], p, "Player not found");
-  //     if (p == 1) {
-  //       counter++;
-  //     }
-  //   }
+      chai.assert.include([0, 1], p, "Player not found");
+      if (p == 1) {
+        counter++;
+      }
+    }
 
-  //   // Counter selects player 1 approximately 50 times (+/- 10)
-  //   chai.assert.approximately(50, counter, 20);
-  // });
+    // Counter selects player 1 approximately 50 times (+/- 10)
+    chai.assert.approximately(50, counter, 20);
+  });
 
-  // test("gets game player username by ID and index", async () => {
-  //   // Add some players to the game
-  //   await Game.findById("1", async (err, game) => {
-  //     game.players = ["test"];
-  //     await game.save();
-  //   });
+  test("gets game player username by ID and index", async () => {
+    // Add some players to the game
+    await Game.findById("1", async (err, game) => {
+      game.players = ["test"];
+      await game.save();
+    });
 
-  //   const player = await getGamePlayer("1", 0);
-  //   chai.expect(player).to.eq("test");
-  // });
+    const player = await getGamePlayer("1", 0);
+    chai.expect(player).to.eq("test");
+  });
 
-  // test("get all game players", async () => {
-  //   // Add some players to the game
-  //   await Game.findById("1", async (err, game) => {
-  //     game.players = ["test 1", "test 2"];
-  //     await game.save();
-  //   });
+  test("get all game players", async () => {
+    // Add some players to the game
+    await Game.findById("1", async (err, game) => {
+      game.players = ["test 1", "test 2"];
+      await game.save();
+    });
 
-  //   // Get all the players
-  //   const players = await getGamePlayers("1");
-  //   chai.expect(players).to.eql(["test 1", "test 2"]);
-  // });
+    // Get all the players
+    const players = await getGamePlayers("1");
+    chai.expect(players).to.eql(["test 1", "test 2"]);
+  });
 
-  // test("update next move", async () => {
-  //   // Check game is set to -1
-  //   await Game.findById("1", async (err, game) => {
-  //     chai.expect(game.nextMove).to.eq(-1);
-  //   });
+  test("update next move", async () => {
+    // Check game is set to -1
+    await Game.findById("1", async (err, game) => {
+      chai.expect(game.nextMove).to.eq(-1);
+    });
 
-  //   // Set game next move to 1
-  //   const updated = await updateNextMove("1", 1);
-  //   chai.expect(updated).to.eq(true);
+    // Set game next move to 1
+    const updated = await updateNextMove("1", 1);
+    chai.expect(updated).to.eq(true);
 
-  //   // Check game is set to 1
-  //   const game = await Game.findById("1").exec();
-  //   chai.assert.equal(game.nextMove, 1, "Next move not updated to 1");
-  // });
+    // Check game is set to 1
+    const game = await Game.findById("1").exec();
+    chai.assert.equal(game.nextMove, 1, "Next move not updated to 1");
+  });
 
-  // test("switch moves ", async () => {
-  //   // Set nextMove to 0
-  //   let game;
-  //   game = await Game.findById("1").exec();
-  //   game.nextMove = 0;
-  //   await game.save();
+  test("switch moves ", async () => {
+    // Set nextMove to 0
+    let game;
+    game = await Game.findById("1").exec();
+    game.nextMove = 0;
+    await game.save();
 
-  //   // Check game is set to -1
-  //   game = await Game.findById("1").exec();
-  //   chai.assert.equal(game.nextMove, 0, "Next move not set to 0");
+    // Check game is set to -1
+    game = await Game.findById("1").exec();
+    chai.assert.equal(game.nextMove, 0, "Next move not set to 0");
 
-  //   // Switch move to 1
-  //   let nextPlayer = -1;
-  //   nextPlayer = await switchMove("1", 0);
-  //   chai.assert.equal(nextPlayer, 1, "Next move not updated to 1");
+    // Switch move to 1
+    let nextPlayer = -1;
+    nextPlayer = await switchMove("1", 0);
+    chai.assert.equal(nextPlayer, 1, "Next move not updated to 1");
 
-  //   // Switch move to 0
-  //   nextPlayer = await switchMove("1", 1);
-  //   chai.assert.equal(nextPlayer, 0, "Next move not updated to 0");
-  // });
+    // Switch move to 0
+    nextPlayer = await switchMove("1", 1);
+    chai.assert.equal(nextPlayer, 0, "Next move not updated to 0");
+  });
 
   suiteTeardown(async () => {
     await disconnectTestDb();
