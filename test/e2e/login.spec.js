15242ced7c2baf5e83074b9dcf79b0bf55d13ce7
Goel Biju <goelmolel@hotmail.com>
2021-01-12 18:03:40 +0000

    Add login UI tests

--- test/e2e/login.spec.js
+++ test/e2e/login.spec.js
@@ -1,12 +1,12 @@
 const puppeteer = require("puppeteer");
 
-suite("UI", function () {
+suite("e2e - register and login", function () {
   let baseUrl = `http://localhost:9000`;
   let browser;
   let page;
 
   suiteSetup(async function () {
-    browser = await puppeteer.launch();
+    browser = await puppeteer.launch({ headless: false });
     page = await browser.newPage();
   });
 
@@ -23,7 +23,31 @@ suite("UI", function () {
   });
 
   test("it should register a user", async function () {
-    await page.goto(baseUrl);
-    console.log(page);
+    await page.goto(baseUrl + "/register");
+    await page.type("#username", "test");
+    await page.type("#password", "test");
+    await page.type("#confirmPassword", "test");
+
+    await page.click("#register-btn");
+
+    await page.goto(baseUrl + "/register");
+    await page.type("#username", "test2");
+    await page.type("#password", "test2");
+    await page.type("#confirmPassword", "test2");
+
+    await page.click("#register-btn");
+  });
+
+  test("it should login a user", async function () {
+    await page.goto(baseUrl + "/login");
+    await page.type("#username", "test");
+    await page.type("#password", "test");
+
+    await Promise.all([
+      page.click("#login-btn"),
+      page.waitForNavigation({ waitUntil: "networkidle0" }),
+    ]);
+
+    await page.click("#logout-btn");
   });
 });
